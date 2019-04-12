a94b01b48f56cfc400f5872ea4295986a03b45a0
yip-jia <499317906@qq.com>
2019-04-12 15:30:58 +0800

    '2'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,10 +1,39 @@
 <template>
-     <div></div>
+  <div></div>
 </template>
 
 
 <script>
+import { getSingerList } from "../../api/singer.js";
+import { ERR_OK } from "../../api/config.js";
+
 export default {
-    
-}
+  data() {
+    return {
+      singers: []
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code == ERR_OK) {
+          this.singers = res.data.list;
+          console.info(this.singers);
+        }
+      });
+    }
+  }
+};
 </script>
+
+
+<style lang="stylus" scoped>
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
