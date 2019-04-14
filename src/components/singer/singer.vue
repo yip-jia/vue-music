580b9ddbe9675276188263862201cc85d36b7e09
yip-jia <499317906@qq.com>
2019-04-14 21:39:24 +0800

    增加singer.js

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,6 +6,9 @@
 <script>
 import { getSingerList } from "../../api/singer.js";
 import { ERR_OK } from "../../api/config.js";
+import Singer from "../../common/js/singer.js";
+const HOT_NAME = "热门";
+const HOT_SINGER_LEN = 10;
 
 export default {
   data() {
@@ -21,9 +24,41 @@ export default {
       getSingerList().then(res => {
         if (res.code == ERR_OK) {
           this.singers = res.data.list;
-          console.info(this.singers);
+          console.info(this._normalizeSinger(this.singers));
         }
       });
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      };
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            })
+          );
+        }
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        map[key].items.push(
+          new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          })
+        );
+      });
+      console.info(map);
     }
   }
 };
@@ -31,9 +66,10 @@ export default {
 
 
 <style lang="stylus" scoped>
-  .singer
-    position: fixed
-    top: 88px
-    bottom: 0
-    width: 100%
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
